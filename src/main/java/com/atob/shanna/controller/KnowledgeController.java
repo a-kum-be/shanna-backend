4a608ba62032f104845b6e76a18d2f6fc801ea17
= <shakev00@gmail.com>
2022-04-02 20:01:18 +0300

    Add CORS

--- src/main/java/com/atob/shanna/controller/KnowledgeController.java
+++ src/main/java/com/atob/shanna/controller/KnowledgeController.java
@@ -5,15 +5,13 @@ import com.atob.shanna.dto.KnowledgeRequestDto;
 import com.atob.shanna.service.KnowledgeService;
 import com.atob.shanna.service.impl.KnowledgeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/knowledge")
+@CrossOrigin(origins = "*")
 public class KnowledgeController {
 
     private final KnowledgeService knowledgeService;
