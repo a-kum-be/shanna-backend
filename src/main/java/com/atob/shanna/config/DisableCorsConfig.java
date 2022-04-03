4d583be8358b13f0a8c26877c7847d6873155fdd
isteffanov <isteffanov@gmail.com>
2022-04-03 10:36:03 +0300

    integrated text detection

---
+++ src/main/java/com/atob/shanna/config/DisableCorsConfig.java
@@ -0,0 +1,14 @@
+package com.atob.shanna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DisableCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
