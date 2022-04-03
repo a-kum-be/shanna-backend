f9cc04428cb5509e4655b1c785fe9b00c27459e3
Georgi Shakev <shakev00@gmail.com>
2022-04-03 12:17:30 +0300

    g

--- src/main/java/com/atob/shanna/controller/DetectionController.java
+++ src/main/java/com/atob/shanna/controller/DetectionController.java
@@ -1,5 +1,6 @@
 package com.atob.shanna.controller;
 
+import com.atob.shanna.dto.KnowledgeResponseDto;
 import com.atob.shanna.service.DetectionService;
 import com.atob.shanna.service.KnowledgeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +9,22 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 @RestController
-@RequestMapping("api/detect")
+@RequestMapping("/api/detect")
 @CrossOrigin(origins = "*")
 public class DetectionController {
 
-    private final DetectionService detectionService;
+    private final KnowledgeService knowledgeService;
 
     @Autowired
-    public DetectionController(DetectionService detectionService) {
-        this.detectionService = detectionService;
+    public DetectionController(KnowledgeService knowledgeService) {
+        this.knowledgeService = knowledgeService;
     }
 
     @PostMapping("/post")
-    public String detect(@RequestParam("resource") MultipartFile image) throws IOException {
-        InputStream is = image.getInputStream();
-        return detectionService.detect(is);
+    public List<KnowledgeResponseDto> detect(@RequestParam("resource") MultipartFile image) throws IOException {
+        return knowledgeService.bulkSave(image);
     }
 }
